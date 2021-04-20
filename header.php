a57ed494003c343b13a0fb73f7c2aec52828e4d8
RobertoMati <74470663+RobertoMati@users.noreply.github.com>
2021-04-20 18:58:05 +0200

    Modificaciones Header

--- header.php
+++ header.php
@@ -1,30 +1,52 @@
+<script>
+    function checkit() {
+        elem = document.getElementById("check");
+        if (elem.checked)
+            elem.checked=false
+        else
+            elem.checked=true
+    }
+</script>
+
 <header>
-    <header>
-        <a class="logo" href="index.php"><img src="img/logo.svg" alt="logo"></a>
-        <nav>
-            <ul class="navegar">
-                <li><a href="index.php">Inicio</a></li>
-                <li><a href="#">Nosotros</a></li>
-                <li><a href="#">Servicios</a></li>
-                <li><a href="contacto.php">Contacto</a></li>
-            </ul>
-        </nav>
-        <a class="inicio" href="iniciosesion.php">Iniciar Sesión</a>
-        <p class="menu inicio">Menú</p>
-    </header>
+
+    <div class="contenedor">
+        <input type="checkbox" name="" id="check" hidden>
+
+        <div class="logo">
+            <a href="index.php"><img src="img/logo.svg" alt="logo" class="img-logo"></a>
+        </div>
+
+        <div class="nav-btn">
+            <div class="nav-links">
+                <ul>
+                    <li class="nav-link" style="--i: 0.3s">
+                        <a href="index.php">Inicio</a>
+                    </li>
+
+                    <li class="nav-link" style="--i: 0.3s">
+                        <a href="#">Nosotros</a>
+                    </li>
+
+                    <li class="nav-link" style="--i: 0.3s">
+                        <a href="#">Servicios</a>
+
+                    </li>
+
+                    <li class="nav-link" style="--i: 0.3s">
+                        <a href="contacto.php">Contacto</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="butolog" style="--i: 0.3s">
+                <a href="iniciosesion.php" class="buto solid">Iniciar Sesión</a>
+            </div>
+        </div>
+
+        <div class="hamburger-menu-container">
+            <a class="picuser" href="iniciosesion.php"><img src="img/bx-user.svg" alt="userpic"></a>
+            <div class="hamburger-menu" onclick="checkit()"></div>
+    </div>
 </header>
 
-<!--aqui empieza el menu desplegable-->
-<div id="menutlf" class="overlay">
-    <a class="close">&times;</a>
-    <div class="overlay__content">
-        <a href="index.php">Inicio</a>
-        <a href="iniciosesion.php">Iniciar Sesión</a>
-        <a href="#">Nosotros</a>
-        <a href="#">Servicios</a>
-        <a href="contacto.php">Contacto</a>
-
-    </div>
-</div>
-<!--aqui termina el menu desplegable-->
-<script type="text/javascript" src="js/mobile.js"></script>
