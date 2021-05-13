d5a4dc82dfc7a476b21a30d063c5245771ceb9ab
Jordi272 <74501189+Jordi272@users.noreply.github.com>
2021-05-13 20:23:52 +0200

    Add files via upload

--- src/enviarcorreo.php
+++ src/enviarcorreo.php
@@ -1,10 +1,12 @@
 <?php
+
+//variable para guardar los datos del formulario
 $nombre = $_POST["nombre"];
 $email = $_POST["email"];
 $mensaje= $_POST["mensaje"];
 
 $body = "Nombre: " . $nombre . "<br>Correo: " . $email . "<br>Mensaje: " . $mensaje;
-
+//librerias
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -14,28 +16,31 @@ require 'api/v1.0/includes/phpmailer/SMTP.php';
 $mail = new PHPMailer(true);
 try {
     //Server settings
-    $mail->SMTPDebug =0 ;                      //Enable verbose debug output
-    $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'formulariosgti@gmail.com';                     //SMTP username
-    $mail->Password   = 'formularios04';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->SMTPDebug =0 ;                      //Errores
+    $mail->isSMTP();                                            //Envair mediante smtp
+    $mail->Host       = 'smtp.gmail.com';                     //Servidor de gmail
+    $mail->SMTPAuth   = true;                                   //Autentificacion SMTP
+    $mail->Username   = 'formulariosgti@gmail.com';                     //SMTP nombre
+    $mail->Password   = 'formularios04';                               //SMTP contraseña
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Encriptacion
+    $mail->Port       = 587;                                    //Puerto
 
     //Recipients
-    $mail->setFrom('formulariosgti@gmail.com', 'GTI');     //Add a recipient
+    $mail->setFrom('formulariosgti@gmail.com', 'GTI');     //Correo que recibe
     $mail->addAddress('tucampogti@gmail.com');
 
 
     //Content
-    $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Formulario de contacto | GTI' ;
-    $mail->Body    = $body;
+    $mail->isHTML(true);
+    $mail->Subject = 'Formulario de contacto | GTI' ;  //Asunto
+    $mail->Body    = $body;   //Se envia lo que ponga el usuario
     $mail->CharSet= 'UTF-8';
     $mail->send();
+
+    //mensajes del servidor
+
     echo '<script>
-    alert("El mensaje se envió correctamente");
+    alert("El mensaje se envió correctamente");   
     //window.history.go(-1);
     location = "contacto.php"
     //document.getElementById("formulario_txt").reset();
