27385f17f0d7f7e2c47222682016bb3ab3285526
RobertoMati <74470663+RobertoMati@users.noreply.github.com>
2021-06-15 09:21:48 +0200

    Add files via upload

--- src/js/miperfil.js
+++ src/js/miperfil.js
@@ -1,25 +1,43 @@
-function getUserInfo(){
+function getUserInfo() {
     fetch('../api/v1.0/sesion', {
         method: 'GET',
-    }).then(function(respuesta) {
+    }).then(function (respuesta) {
         if (respuesta.ok) {
             return respuesta.json();
         }
-    }).then(function(data) {
-        fetch('../api/v1.0/user/'+data['id'], {
+    }).then(function (data) {
+        fetch('../api/v1.0/user/' + data['id'], {
             method: 'GET',
-        }).then(function(respuesta) {
+        }).then(function (respuesta) {
             if (respuesta.ok) {
                 return respuesta.json();
             }
-        }).then(function(data) {
-            document.getElementById('nombre_usuario').innerText = data[0]['Apodo']
-            document.getElementById('telefono_usuario').innerText = data[0]['telefono']
-            document.getElementById('correo_usuario').innerText = data[0]['correo']
-            document.getElementById('servicios_usuario').innerText = data[0]['fechaAlta']
-            document.getElementById('direccion_usuario').innerText = data[0]['Direccion']
+        }).then(function (data) {
+            // document.getElementById('nombre_usuario').innerText = data[0]['Apodo']
+            // document.getElementById('telefono_usuario').innerText = data[0]['telefono']
+            // document.getElementById('correo_usuario').innerText = data[0]['correo']
+            // document.getElementById('servicios_usuario').innerText = data[0]['fechaAlta']
+            // document.getElementById('direccion_usuario').innerText = data[0]['Direccion']
+            llenarTabla()
         })
-
     })
+}
 
+function llenarTabla() {
+
+    fetch('../api/v1.0/user/', {
+        method: 'GET',
+    }).then(function (respuesta) {
+        if (respuesta.ok) {
+            return respuesta.json();
+        }
+    }).then(function (data) {
+        document.getElementById("tablaUsuarios")
+        console.log(data)
+        // document.getElementById('nombre_usuario').innerText = data[0]['Apodo']
+        // document.getElementById('telefono_usuario').innerText = data[0]['telefono']
+        // document.getElementById('correo_usuario').innerText = data[0]['correo']
+        // document.getElementById('servicios_usuario').innerText = data[0]['fechaAlta']
+        // document.getElementById('direccion_usuario').innerText = data[0]['Direccion']
+    })
 }
