c1f032765e91b916772b1b9ee4d6011802b1f672
RobertoMati <74470663+RobertoMati@users.noreply.github.com>
2021-06-16 12:08:05 +0200

    Nueva estructura Footer

---
+++ src/css/footer.php
@@ -0,0 +1,41 @@
+<?php if (!isset($path)) $path = "./"?>
+<footer id="contenedor-footer">
+
+<!-- ------------------------------------------------------------------------------------------------------------------------ -->
+
+        <nav class="contenedor-paginas">
+            <ul>
+                <li class="enlacesfooternpf">
+                    <a href="<?php echo $path?>nosotros.php">Nosotros</a>
+                </li>
+                <li>
+                    <a href="<?php echo $path?>FAQ.php">Preguntas Frecuentes</a>
+                </li>
+            </ul>
+        </nav>
+        <!--    AQUI EMPIEZA EL CONTENEDOR DE ICONOS DE REDES SOCIALES -->
+        <nav class="contenedor-redes">
+            <ul>
+                <li>
+                    <a class="redes" href="https://www.facebook.com/empresa.gti.3/" target="_blank">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+                </li>
+                <li>
+                    <a class="redes" href="https://twitter.com/GInteractivas" target="_blank">
+                        <i class="fab fa-twitter"></i>
+                    </a>
+                </li>
+                <li>
+                    <a class="redes" href="https://www.instagram.com/empresagti/?hl=es" target="_blank">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!--    AQUI TERMINA EL CONTENEDOR DE ICONOS DE REDES SOCIALES -->
+
+<!-- ------------------------------------------------------------------------------------------------------------------------ -->
+        <!--    TEXTO DE COPYRIGHT-->
+		<p id="copyright">Copyright &#169 2021 GTI Todos los derechos reservados</p>
+	</footer>
