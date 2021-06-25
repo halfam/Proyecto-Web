eada2f03958e006f169a1ef2e55de781e548ef75
Joan <jaltbri@epsg.upv.es>
2021-06-25 21:26:29 +0200

    Mostrar dos sondas en la misma grafica

--- src/app/graficas.php
+++ src/app/graficas.php
@@ -3,43 +3,48 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Información detallada</title>
     <link rel="stylesheet" href="../css/estilo-header_footer.css">
     <link rel="stylesheet" href="../css/estilo-graficas.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@600;800&family=Varela+Round&display=swap" rel="stylesheet">
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.2.0/chart.min.js" integrity="sha512-VMsZqo0ar06BMtg0tPsdgRADvl0kDHpTbugCBBrL55KmucH6hP9zWdLIWY//OTfMnzz6xWQRxQqsUFefwHuHyg==" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@600;800&family=Varela+Round&display=swap"
+          rel="stylesheet">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.2.0/chart.min.js"
+            integrity="sha512-VMsZqo0ar06BMtg0tPsdgRADvl0kDHpTbugCBBrL55KmucH6hP9zWdLIWY//OTfMnzz6xWQRxQqsUFefwHuHyg=="
+            crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/luxon@1.26.0/build/global/luxon.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-luxon@1.0.0/dist/chartjs-adapter-luxon.min.js"></script>
 
     <script src="https://kit.fontawesome.com/a81368914c.js"></script>
     <script src="../js/restringir-acceso.js"></script>
 
-<!--    bootstrapp -->
-<!--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">-->
-<!--    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>-->
-<!--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>-->
-
+    <!--    bootstrapp -->
+    <!--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">-->
+    <!--    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>-->
+    <!--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>-->
 
 
 </head>
 
 <body>
 
-    <?php
-    $path = "../";
-    include_once $path.'header.php';?>
-    <div class="header"></div>
-    <section class="cabecera_princ" >
-        <div class="graficas_princ">
-            <div class="dentro"><h1>Información detallada</h1></div>
-            <img src="../img/grafic.png" alt="img-graficas" id="graficas-img">
-            <img src="../img/grafic2.png" alt="img-graficas" id="graficas-img2">
-            <img src="../img/grafic3.png" alt="img-graficas" id="graficas-img3">
-            <div class="interracion-usuario">
-                <a href="miscampos.php" class="flecha_atras_bonita_bonita"><i class="fas fa-arrow-circle-left"></i></a>
+<?php
+$path = "../";
+include_once $path . 'header.php'; ?>
+<div class="header"></div>
+<section class="cabecera_princ">
+    <div class="graficas_princ">
+        <div class="dentro"><h1>Información detallada</h1></div>
+        <img src="../img/grafic.png" alt="img-graficas" id="graficas-img">
+        <img src="../img/grafic2.png" alt="img-graficas" id="graficas-img2">
+        <img src="../img/grafic3.png" alt="img-graficas" id="graficas-img3">
+        <div class="interracion-usuario">
+            <a href="miscampos.php" class="flecha_atras_bonita_bonita"
+               onclick="borrarSesion()"><i
+                        class="fas fa-arrow-circle-left"></i></a>
             <p class="mostrar-mis-datos">Mostrar datos desde: </p>
             <select name="tiempo" id="tiempo" onchange="cambiarTiempo(this.value)">
                 <option value="1">1 día</option>
@@ -47,36 +52,74 @@
                 <option value="30">1 mes</option>
                 <option value="365">1 año</option>
             </select>
-            </div>
         </div>
-    </section>
-    <div class="graficas" id="base_graficas"></div>
-    <div class="comparar" >
-        <i class="fas fa-plus-circle fa-2x" onclick="nuevaSonda()"></i>
     </div>
-    <?php include_once $path.'footer.php' ?>
+</section>
+<div class="graficas" id="base_graficas"></div>
+<div class="comparar">
+    <i class="fas fa-plus-circle fa-2x" onclick="mostrarComparador()"></i>
+</div>
 
 
+<div class="overlay_graficas ocultar" id="comparador">
+    <!-- -------------------------------------------------------------------------------------------------------------------------- -->
+    <!-- FORMULARIO LOGIN -->
+    <p>Seleccione el tipo de medida</p>
+    <!--    <form>-->
+    <div class="formularioComparar">
+        <select id="test-dropdown">
+            <option value="3">Temperatura</option>
+            <option value="1">Salinidad</option>
+            <option value="0">Humedad</option>
+            <option value="2">Luminosidad</option>
+        </select>
+        <!--        <input type="submit" name="aceptar" id="botonAceptar" onclick="nuevaSonda()">-->
+        <button onclick="nuevaSonda()"> Enviar</button>
+    </div>
+    <!--    </form>-->
+    <!-- -------------------------------------------------------------------------------------------------------------------------- -->
+    <!-- FIN DE PORMULARIO LOGIN -->
+</div>
 
-    <script src="../js/grafica.js"></script>
-    <script>
-        function checkSondas(){
-            let aux = sessionStorage.getItem('sondas').split(',')
-            aux.forEach(function (sonda) {
-                idSondas.push(parseInt(sonda))
-            })
-            for (let i = 0; i < idSondas.length; i++) {
-                if (!isNaN(idSondas[i]))
+
+<?php include_once $path . 'footer.php' ?>
+
+
+<script src="../js/grafica.js"></script>
+<script>
+    function checkSondas() {
+        let aux = sessionStorage.getItem('sondas').split(',')
+        aux.forEach(function (sonda) {
+            idSondas.push(parseInt(sonda))
+        })
+        for (let i = 0; i < idSondas.length; i++) {
+            if (!isNaN(idSondas[i]))
                 cargarMediciones(idSondas[i]);
-            }
         }
-        checkSondas();
-        function nuevaSonda(){
-            if (idSondas.length < 2 ){
-                location.href = 'miscampos.php'
-            }
-        }
-    </script>
+    }
+
+    checkSondas();
+
+    function nuevaSonda() {
+        almacenarDatosSonda()
+        // if (idSondas.length < 2) {
+        let select = document.getElementById("test-dropdown")
+        let valor = select.options[select.selectedIndex].value
+        console.log(valor)
+        sessionStorage.setItem("medicionComparar", valor)
+        location.href = 'miscampos.php'
+        // }
+    }
+
+    function mostrarComparador() {
+        var comparador = document.getElementById("comparador")
+        comparador.classList.remove("ocultar")
+    }
+
+    function borrarSesion() {
+        sessionStorage.removeItem('medicionComparar')
+    }
+</script>
 
 </body>
 
--- src/js/grafica.js
+++ src/js/grafica.js
@@ -10,86 +10,6 @@ let idSondas = [];
 let tiempo = 7;
 let auxTiempo = 0;
 
-function cargarMediciones(id) {
-    fetch(path + 'api/v1.0/modelos/get-mediciones.php?id=' + id + "&tiempo=" + 365, {
-        method: 'GET',
-    }).then(function (respuesta) {
-        if (respuesta.ok) {
-            return respuesta.json();
-        }
-    }).then(function (data) {
-        if (data == null) {
-            data = [];
-            data.push({
-                idSonda: id,
-                idPosicion: "",
-                fecha: null,
-                temperatura: 0,
-                humedad: 0,
-                salinidad: 0,
-                luminosidad: 0
-            })
-            let selectTiempo = document.getElementById("tiempo");
-            switch (auxTiempo) {
-                case 0:
-                    cambiarTiempo(7);
-                    auxTiempo++;
-                    selectTiempo.options[1].setAttribute("selected", true)
-                    break;
-                case 1:
-                    cambiarTiempo(30);
-                    auxTiempo++;
-                    selectTiempo.options[2].setAttribute("selected", true)
-                    break;
-                case 2:
-                    cambiarTiempo(365);
-                    auxTiempo++;
-                    selectTiempo.options[3].setAttribute("selected", true)
-                    break;
-            }
-        }
-
-
-        procesarDatos(data);
-    })
-}
-
-function procesarDatos(mediciones) {
-    mediciones = mediciones.sort(function (a, b) {
-        if (a.fecha < b.fecha) return -1;
-        if (a.fecha > b.fecha) return 1;
-        return 0;
-    }); // recorrer las mediciones
-    let fechas = [];
-    let temperatura = [];
-    let luminosidad = [];
-    let salinidad = [];
-    let humedad = [];
-    let id;
-
-    mediciones.forEach(function (medicion) {
-
-        let i = fechas.indexOf(medicion.fecha);
-        if (i < 0) {
-            fechas.push(medicion.fecha);
-            temperatura.push(parseFloat(medicion.temperatura));
-            luminosidad.push(parseFloat(medicion.luminosidad));
-            salinidad.push(parseFloat(medicion.salinidad));
-            humedad.push(parseFloat(medicion.humedad));
-            id = parseFloat(medicion.idSonda);
-        }
-    })
-    datos.labels = fechas;
-    datos.datasets[0].data = humedad;
-    datos.datasets[1].data = salinidad;
-    datos.datasets[2].data = luminosidad;
-    datos.datasets[3].data = temperatura;
-    opciones.plugins.title.text = "Los datos obtenidos por la sonda " + id;
-
-    dts.push(datos)
-    ops.push(opciones)
-    CrearGrafica(id);
-}
 var dts = []
 var ops = []
 
@@ -165,6 +85,147 @@ let opciones = {
 };
 var miGrafica;
 
+
+
+function cargarMediciones(id) {
+    fetch(path + 'api/v1.0/modelos/get-mediciones.php?id=' + id + "&tiempo=" + 365, {
+        method: 'GET',
+    }).then(function (respuesta) {
+        if (respuesta.ok) {
+            return respuesta.json();
+        }
+    }).then(function (data) {
+        if (data == null) {
+            data = [];
+            data.push({
+                idSonda: id,
+                idPosicion: "",
+                fecha: null,
+                temperatura: 0,
+                humedad: 0,
+                salinidad: 0,
+                luminosidad: 0
+            })
+            let selectTiempo = document.getElementById("tiempo");
+            switch (auxTiempo) {
+                case 0:
+                    cambiarTiempo(7);
+                    auxTiempo++;
+                    selectTiempo.options[1].setAttribute("selected", true)
+                    break;
+                case 1:
+                    cambiarTiempo(30);
+                    auxTiempo++;
+                    selectTiempo.options[2].setAttribute("selected", true)
+                    break;
+                case 2:
+                    cambiarTiempo(365);
+                    auxTiempo++;
+                    selectTiempo.options[3].setAttribute("selected", true)
+                    break;
+            }
+        }
+
+
+        procesarDatos(data);
+    })
+}
+
+function procesarDatos(mediciones) {
+    mediciones = mediciones.sort(function (a, b) {
+        if (a.fecha < b.fecha) return -1;
+        if (a.fecha > b.fecha) return 1;
+        return 0;
+    }); // recorrer las mediciones
+    let fechas = [];
+    let temperatura = [];
+    let luminosidad = [];
+    let salinidad = [];
+    let humedad = [];
+    let id;
+
+    mediciones.forEach(function (medicion) {
+
+        let i = fechas.indexOf(medicion.fecha);
+        if (i < 0) {
+            fechas.push(medicion.fecha);
+            temperatura.push(parseFloat(medicion.temperatura));
+            luminosidad.push(parseFloat(medicion.luminosidad));
+            salinidad.push(parseFloat(medicion.salinidad));
+            humedad.push(parseFloat(medicion.humedad));
+            id = parseFloat(medicion.idSonda);
+        }
+    })
+
+    datos.labels = fechas;
+    if (sessionStorage.getItem("medicionComparar")!=null){
+        var tipoComparar = sessionStorage.getItem("medicionComparar")
+        var datosPrevios = JSON.parse(sessionStorage.getItem("datosPrevios"))
+        tipoComparar ="3"
+        let tipo = ""
+        switch (tipoComparar){
+            case "0":
+                tipo = "Humedad"
+                datos.datasets[1].data = [];
+                datos.datasets[1] = datosPrevios.datasets[0]
+                datos.datasets[2].data = []
+                // datos.datasets[3].data = []
+                modificarDatos()
+
+                break;
+            case "1":
+                tipo = "Salinidad"
+                datos.datasets[0].data = [];
+                datos.datasets[0] = datosPrevios.datasets[1]
+                datos.datasets[2].data = []
+                // datos.datasets[3].data = []
+                modificarDatos()
+                break;
+            case "2":
+                tipo = "Luminosidad"
+                datos.datasets[1].data = [];
+                // datos.datasets[1] = datos.datasets[2];
+                datos.datasets[0].data = [];
+                // datos.datasets[3].data = [];
+                datos.datasets[0] = datosPrevios.datasets[2]
+                // datos.datasets[1]={}
+                modificarDatos()
+                break;
+            case "3":
+                tipo = "Temperatura"
+                datos.datasets[1].data = temperatura;
+                // datos.datasets[2].data = [];
+                // datos.datasets[0] = [];
+                datos.datasets[0] = datosPrevios.datasets[3]
+                modificarDatos()
+                // datos.datasets[1] = datos.datasets[3]
+                // datos.datasets[1] ={}
+                break;
+        }
+
+        opciones.plugins.title.text = "Comparando " + tipo  +" de las sondas " + idSondas[0] + " y " + idSondas[1];
+        // datos.datasets.pop()
+        // datos.datasets.pop()
+
+        // console.log(datos.datasets)
+    }
+    else{
+
+        datos.datasets[0].data = humedad;
+        datos.datasets[1].data = salinidad;
+        datos.datasets[2].data = luminosidad;
+        datos.datasets[3].data = temperatura;
+        opciones.plugins.title.text = "Los datos obtenidos por la sonda " + id;
+    }
+    dts.push(datos)
+    console.log(dts[0])
+    ops.push(opciones)
+    if (miGrafica== null || miGrafica == undefined) {
+        CrearGrafica(id);
+    }
+    miGrafica.update()
+}
+
 function CrearGrafica(idSonda) {
 
     var grafica = document.getElementById("chart" + idSonda)
@@ -197,7 +258,46 @@ function CrearGrafica(idSonda) {
 
 }
 
+function modificarDatos() {
+    datos = {
+        labels: [],
+        datasets: [
+            {
+                label: "sonda " + idSondas[0] ,
+                backgroundColor: 'rgb(0,225,0)',
+                borderColor: "rgb(0,225,0)",
+                tension: 0.3,
+                pointStyle: 'point',
+                pointRadius: 5,
+            },
+            {
+                label: "sonda " + idSondas[1],
+                backgroundColor: 'rgb(255,90,0)',
+                borderColor: 'rgb(255,90,0)',
+                tension: 0.3,
+                pointStyle: 'point',
+                pointRadius: 5,
+            }
+        ]
+    };
+}
 
+function almacenarDatosSonda() {
+
+    datos.datasets[0].backgroundColor = "rgb(0,225,0)"
+    datos.datasets[0].borderColor = "rgb(0,225,0)"
+    datos.datasets[0].label = "sonda " + idSondas[0]
+    datos.datasets[1].backgroundColor = "rgb(0,225,0)"
+    datos.datasets[1].borderColor = "rgb(0,225,0)"
+    datos.datasets[1].label = "sonda " + idSondas[0]
+    datos.datasets[2].backgroundColor = "rgb(0,225,0)"
+    datos.datasets[2].borderColor = "rgb(0,225,0)"
+    datos.datasets[2].label = "sonda " + idSondas[0]
+    datos.datasets[3].backgroundColor = "rgb(0,225,0)"
+    datos.datasets[3].borderColor = "rgb(0,225,0)"
+    datos.datasets[3].label = "sonda " + idSondas[0]
+    sessionStorage.setItem("datosPrevios",JSON.stringify(datos))
+}
 function cambiarTiempo(value) {
     tiempo = parseInt(value);
     checkSondas();
