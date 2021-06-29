8aa644a238ab42b5d6262f3a6e4d8a03ade8c3bf
Jordi272 <74501189+Jordi272@users.noreply.github.com>
2021-06-29 17:08:41 +0200

    Add files via upload

--- src/forgotPassword.php
+++ src/forgotPassword.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,shrink-to-fit=no,initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Nosotros</title>
+    <title>Recuperar Contraseña</title>
     <script src="https://kit.fontawesome.com/a81368914c.js"></script>
     <link rel="stylesheet" href="css/estilo-header_footer.css">
 <!--    <link rel="stylesheet" href="css/nosotros.css">-->
@@ -16,18 +16,21 @@
 <body>
 <?php include_once 'header.php'?>
 <div class="header"></div>
-<h2>Recuperar contraseña</h2>
-<div class="container">
-    <div class="regisFrm">
+<section class="forgotPassword">
+<h1 class="titulo-recuperarContra">Recuperar contraseña</h1>
+    <p class="texto-descriptivo1">Si ha olvidado su contraseña deberá rellenar el siguiente formulario para poder reestablecerla.</p>
+        <div class="formulario-correo-contra">
         <form method="post" onsubmit="recuperar()" >
-            <input type="email" name="correo" placeholder="correo" required id="Recuperar_correo">
+            <p class="text-correo-electro">Correo electrónico:</p>
+            <input type="email" name="correo" placeholder="correo electrónico" required id="Recuperar_correo">
 <!--            <div class="send-button">-->
             <br>
-            <input type="submit" name="forgotSubmit" value="CONTINUE" >
-<!--            </div>-->
+            <input type="submit" name="forgotSubmit" value="Enviar" class="boton-enviar-correo">
+        </div>
         </form>
-    </div>
-</div>
+        <p class="texto-descriptivo2">Recibirá inmediatamente un correo  con un enlace para restablecer la contraseña de su cuenta.</p>
+    <img class="img-relleno" src="img/Forgot%20password-bro.svg" alt="Olvidar contraseña">
+</section>
 <script src="js/recuperar.js"></script>
 <!--FOOTER -->
 <?php include 'footer.php' ?>
