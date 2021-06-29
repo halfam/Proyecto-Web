de9a523571b657f085cd817ae058870a503e07f1
Jordi272 <74501189+Jordi272@users.noreply.github.com>
2021-06-29 16:56:37 +0200

    Add files via upload

--- src/recuperar.php
+++ src/recuperar.php
@@ -2,31 +2,42 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width,shrink-to-fit=no,initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Reestablecer contraseña | GTI</title>
+    <script src="https://kit.fontawesome.com/a81368914c.js"></script>
     <link rel="stylesheet" href="css/estilo-header_footer.css">
+    <!--    <link rel="stylesheet" href="css/nosotros.css">-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Rubik:wght@600;800&family=Varela+Round&display=swap" rel="stylesheet">
 </head>
 <body>
 <?php
 $path = './';
 include_once $path . 'header.php';?>
 <div class="header"></div>
-<h2>Recuperar contraseña</h2>
-<div class="container">
+<section class="forgotPassword">
+<h2 class="titulo-recuperarContra">Recuperar contraseña</h2>
+<p class="texto-descriptivo3">Rellene el siguiente formulario si desea reestablecer una nueva conrtaseña para su cuenta.</p>
+<div class="contenedor-recuperar">
     <div class="regisFrm">
         <form method="post" onsubmit="cambiarContrasenya()">
-            <input type="password" name="contraseña" placeholder="Contraseña" required id="Contrasenya1">
-            <input type="password" name="confirmar_contraseña" placeholder=" Confirmar contraseña" required id="Contrasenya2">
+            <p class="texto-contra">Contraseña:</p>
+            <input class="contraseña-recuperar" type="password" name="contraseña" placeholder="Contraseña" required id="Contrasenya1">
+            <p class="texto-contra">Confirmar contraseña:</p>
+            <input class="contraseña-recuperar" type="password" name="confirmar_contraseña" placeholder=" Confirmar contraseña" required id="Contrasenya2">
             <div class="send-button">
-                <input type="submit" name="forgotSubmit" value="CONTINUE" >
+                <input type="submit" name="forgotSubmit" value="Reestablecer" class="boton-enviar-correo">
             </div>
         </form>
     </div>
 </div>
+<p class="texto-descriptivo4">Al pulsar el botón de reestablecer, se guardará automáticamente como nueva contraseña de la cuenta.</p>
+</section>
 <script src="js/recuperar.js"></script>
-
+<!--FOOTER -->
+<?php include 'footer.php' ?>
 </body>
 </html>
 
